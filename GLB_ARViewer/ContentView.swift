0402f6adbdefece0c1918b8335c918124fa70bec
KEN1965 <hammer528@msn.com>
2026-02-10 10:52:39 +0900

    Initial Commit

---
+++ GLB_ARViewer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GLB_ARViewer
+//
+//  Created by Kenichi Takahama on 2026/02/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GLB_ARViewer/GLB_ARViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GLB_ARViewerApp.swift
+//  GLB_ARViewer
+//
+//  Created by Kenichi Takahama on 2026/02/10.
+//
+
+import SwiftUI
+
+@main
+struct GLB_ARViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
